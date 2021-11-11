dba2d2632df810899db78360abb004960951d115
mailt <mailt@Yunus>
2021-11-11 21:48:05 +0530

    commit 3 with test ng

--- SelProjects/src/main/java/week2/day2/CreateLead.java
+++ SelProjects/src/main/java/week2/day2/CreateLead.java
@@ -4,13 +4,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
 
 import io.cucumber.datatable.dependency.com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class CreateLead {
-
-	public static void main(String[] args) throws InterruptedException {
+	
+	@Test
+	public void runLead() {
 		WebDriverManager.chromedriver().setup();
 	//	System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
 		
@@ -25,23 +27,23 @@ public class CreateLead {
          driver.findElement(By.className("crmsfa")).click();
          driver.findElement(By.linkText("Create Lead")).click();
          // driver.findElement(By.xpath("Create Lead")).click();
-         driver.findElement(By.id("createLeadForm_companyName")).sendKeys("Wipro");
-         driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Mohammed");
+         driver.findElement(By.id("createLeadForm_companyName")).sendKeys("TestLeaf");
+         driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Test");
          driver.findElement(By.id("createLeadForm_lastName")).sendKeys("A");
-         driver.findElement(By.className("smallSubmit")).click();
-         Thread.sleep(2000);
+         
+         //Thread.sleep(2000);
          //driver.findElement(By.linkText("Delete")).click();
 
          WebElement market = driver.findElement(By.id("createLeadForm_marketingCampaignId"));
          Select mc= new Select(market);
          mc.selectByIndex(1);
-         
+         driver.findElement(By.className("smallSubmit")).click();
          
          /*WebElement market = driver.findElement(By.id("createLeadForm_marketingCampaignId"));
          Select mc= new Select(market);
          mc.selectByIndex(1);
          */
-         		        
+         		        driver.close();
 	}
 
 }
